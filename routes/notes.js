f9cb07f4e238268c558537b992e0273cc251c2b3
ptrcao <ptrcao@gmail.com>
2023-02-07 21:44:54 +1100

    Re-create project with package.json in root to comply with Heroku deployment requirements

---
+++ routes/notes.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+
+const fs = require("fs");
+const path = require("path");
+
+
+router.get("/", (req, res) => {
+  fs.readFile("db/db.json", "utf-8", (err, data) => {
+    // console.log(data);
+    if (err) throw err;
+    let parsedData = JSON.parse(data);
+    return res.json(parsedData);
+  });
+});
+
+
+// Required for app.post('/api/notes', ...) routes
+const uuid = require('uuid');
+router.use(express.json())
+
+router.post("/", (req, res) => {
+  console.log("req is: " + JSON.stringify(req.body));
+
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    const newNote = {
+      id: uuid.v1(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+    console.log(newNote);
+    notes.push(newNote);
+    fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json(newNote);
+    });
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    const updatedNotes = notes.filter((note) => note.id !== req.params.id);
+    console.log(updatedNotes)
+    fs.writeFile("db/db.json", JSON.stringify(updatedNotes), (err) => {
+      if (err) throw err;
+      res.json(updatedNotes);
+    });
+  });
+});
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,44 @@
+// This application uses the Express.js framework to create a REST API that allows clients to perform CRUD (create, read, update, delete) operations on notes stored in a JSON file. The API has three endpoints:
+
+// - `GET /notes` retrieves a list of all notes stored in the `notes.json` file.
+// - `POST /notes` adds a new note to the list of notes stored in the `notes.json` file. The new note data is passed in the request body as JSON.
+// - `DELETE /notes/:id` deletes a note from the list of notes stored in the `notes.json` file. The ID of the note to delete is passed in the URL as a path parameter.
+
+
+const express = require("express");
+const path = require("path");
+
+
+
+// const bootstrap = require('bootstrap')
+// import 'bootstrap/dist/css/bootstrap.css';
+const app = express();
+
+const PORT = 3001;
+
+// EJS (alternatively use Pug for dynamic HTML)
+// https://www.youtube.com/watch?v=yXEesONd_54
+
+// register view engine
+// app.set("view engine", "ejs");
+// inform ejs which is your views folder because you don't use the default folder name views, but rather 'public'
+// app.set("views", path.join(__dirname,"public"));
+
+app.use(express.static(path.join(__dirname,"public")));
+// app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist")));
+
+
+app.use('/notes',(req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// Include api_notes_router.js for the endpoint /api/notes
+const apiNotesRouter = require('./routes/notes')
+app.use('/api/notes', apiNotesRouter)
+
+app.use('*',(req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
